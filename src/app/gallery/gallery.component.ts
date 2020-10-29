c5373d52545539919436642a57f5f05236ca51df
Angad Singh <asvirdi2522@gmail.com>
2020-10-30 04:10:59 +0530

    iw

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styles: ['./gallery.component.css'
+  styles: ['./gallery.component.html'
   ]
 })
 export class GalleryComponent implements OnInit {
