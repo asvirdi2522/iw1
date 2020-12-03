89dbd25b00a92c3e018c4d9bb78df15e02dcddd4
Angad Singh <asvirdi2522@gmail.com>
2020-12-03 21:25:51 +0530

    iw

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: 'home',             component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'gallery', component: GalleryComponent}
+  { path: 'iwsc', component: GalleryComponent}
 ];
 
 @NgModule({
